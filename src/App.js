1515ef30e2a331ec7866ba0343cdf16abd9273c7
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2023-11-26 03:53:58 +0100

    added redirect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 //LIBRARY IMPORTS
 import React from 'react';
-import { Routes, Route } from "react-router-dom/";
+import { Routes, Route } from "react-router-dom";
 
 import {ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
-// CSS IMPORTS
-import Styles from './index.css';
+
 
 // COMPONENT IMPORTS
 import Login from './pages/auth/Login';
@@ -21,17 +20,17 @@ import RegisterComplete from './pages/auth/RegisterComplete';
 const App = () => {
   return (
     <>
-    <Header />
-    <ToastContainer />
-    <Routes>
-      <Route exact path="/" Component={Home} />
-      <Route exact path="/login" Component={Login} />
-      <Route exact path="/register" Component={Register} />
-      <Route exact path="/register/complete" Component={RegisterComplete} />
-    </Routes>
+      <Header />
+      <ToastContainer />
+      <Routes>
+        <Route exact path="/" Component={Home} />
+        <Route exact path="/login" Component={Login} />
+        <Route exact path="/register" Component={Register} />
+        <Route exact path="/register/complete" Component={RegisterComplete} render={(props) => <RegisterComplete {...props} />} />
+      </Routes>
     </>
   );
 };
 
-//make the component available to other components for importing
+// make the component available to other components for importing
 export default App;
--- src/pages/auth/RegisterComplete.js
+++ src/pages/auth/RegisterComplete.js
@@ -1,73 +1,102 @@
 import React, { useState, useEffect } from "react";
 import { auth } from "../../config/firebase";
-import { sendSignInLinkToEmail } from "firebase/auth";
-
-import {toast } from 'react-toastify';
-
-// we destructure to get the props.history from App component
-const RegisterComplete = (history) => {
-
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+import { signInWithEmailLink } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 
-    // useEffect hook runs when the component mounts for the first time
-    // array at the end is called dependency array 
+const RegisterComplete = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let navigate = useNavigate();
+  
 
-    useState(() => {
-        //console.log(window.localStorage.getItem('emailForRegistration'));
+  // useEffect hook runs when the component mounts for the first time
+  // array at the end is called dependency array
+  useEffect(() => {
+    // console.log("LOCAL STORAGE EMAIL: ", window.localStorage.getItem("emailForRegistration"));
+    // console.log("WINDOW LOCATION URL: ", window.location.href);
 
-        //populate email field as soon as it mounts
-        setEmail(window.localStorage.getItem('emailForRegistration'));
-    }, [])
+    //populate email field as soon as it mounts
+    setEmail(window.localStorage.getItem("emailForRegistration"));
+  }, []);
 
-    const handleSubmit = async (e) => {
-        // prevents browser from reload
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    // prevents browser from reload
+    e.preventDefault();
+    
+      //validation of email & password fields
+      if (!email) {
+          toast.error("email and password is required");
+          return;
+      }
 
-        //we use firbease method to sign user in
+      // if (password.length < 6) {
+      //     toast.error("Password must be at least 6 characters long");
+      //     return;
+      // }
 
 
 
+    //we use firebease method(signinwithuser) to sign user in
+    try {
+        const result = await signInWithEmailLink(auth, email, window.location.href);
+        // console.log("FIREBASE LOGIN RESULT", result);
+
+        // we'll check if email has been verified
+        if (result.user.emailVerified) {
+
+            // remove user email from local storage
+            window.localStorage.removeItem("emailForRegistration");
+
+            //get user id token(new token)
+            const user = auth.currentUser;
+          
+            const idTokenResult = await user.getIdTokenResult();
+
+            console.log("CURRENT USER", user, "IDTOKENRESULT",idTokenResult);
+
+
+
+          // create redux store(dispatch to redux store)
+            
+
+
+          // redirect
+          navigate("/");
+
+        }
+
+    } catch (error) {
+        console.log("ERROR", error);
+        toast.error(error.message);
     }
+  };
 
-    const completeRegistrationForm = () => (
-        <form onSubmit={handleSubmit}>
-            <input 
-                type="email"
-                className="form-control"
-                value={email}
-                
-                disabled
-            />
+  const completeRegistrationForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input type="email" className="form-control" value={email} disabled />
 
-            <input 
-                type="password"
-                className="form-control"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-                autoFocus
-                placeholder="Password "
-            />
+      <input type="password" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} autoFocus placeholder="Password"
+      />
 
-            <button type="submit" className="btn btn-info">
-                Register
-            </button>
-        </form>
-    );
+      <button type="submit" className="btn btn-info">
+        Register
+      </button>
+    </form>
+  );
 
+  return (
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Complete Registration </h4>
 
-    return (
-        <div className="container p-5">
-            <div className="row">
-                <div className="col-md-6 offset-md-3">
-                    <h4>Complete Registration </h4>
-                    
-                    { completeRegistrationForm() }
-                </div>
-            </div>
+          {completeRegistrationForm()}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 //make the component available to other components for importing
